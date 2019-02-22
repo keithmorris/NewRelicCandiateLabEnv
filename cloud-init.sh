6b0c210daada339e5820e20a9d34d54d546c4196
Dave Murphy <davemurphy@newrelic.com>
2019-02-22 14:27:17 -0800

    Updates

--- cloud-init.sh
+++ cloud-init.sh
@@ -6,10 +6,28 @@ users:
   - default
   - name: ${username}
     gecos: New Relic Candidate
+    sudo: ALL=(ALL) NOPASSWD:ALL
     expiredate: ${pw_expiration}
     passwd: ${password}
+  - name: tomcat
+    system: true
+    sudo: ALL=(ALL) NOPASSWD:ALL
 package_upgrade: true
 packages:
   - maven
-  - openjdk-8-jdk
   - git
+  - mysql-server
+runcmd:
+  - sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+  - (cd /tmp && curl -O http://apache.mirrors.tds.net/tomcat/tomcat-9/v9.0.16/bin/apache-tomcat-9.0.16.tar.gz)
+  - mkdir /opt/tomcat
+  - tar xzvf /tmp/apache-tomcat-9.0.16.tar.gz -C /opt/tomcat --strip-components=1
+  - chgrp -R tomcat /opt/tomcat
+  - chmod -R g+r /opt/tomcat/conf
+  - chmod g+x /opt/tomcat/conf
+  - chown -R tomcat /opt/tomcat/webapps /opt/tomcat/work /opt/tomcat/temp /opt/tomcat/logs
+  - iptables -t nat -A OUTPUT -o lo -p tcp --dport 80 -j REDIRECT --to-port 8080
+  - git clone https://github.com/nrcandidatelab/ShopizerJavaApp /tmp/Shopizer
+  - cd /tmp/Shopizer
+  - reboot
+  
