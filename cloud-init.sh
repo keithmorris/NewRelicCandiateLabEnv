48d73e745d487af6f21f693cff2dd0253a8e4c15
Dave Murphy <davemurphy@newrelic.com>
2019-03-11 15:18:10 -0700

    Fix JAVA_HOME environment variable

--- cloud-init.sh
+++ cloud-init.sh
@@ -11,7 +11,6 @@ users:
     ssh_authorized_keys:
       - ${ssh_key}
   - name: tomcat
-    system: true
     sudo: ALL=(ALL) NOPASSWD:ALL
 package_upgrade: true
 packages:
@@ -22,6 +21,23 @@ packages:
   - netfilter-persistent
   - openjdk-8-jdk
 runcmd:
+  - touch /etc/profile.d/00-java.sh
+  - echo \"export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64\" | tee -a \"/etc/profile.d/00-java.sh\"
+  - echo \"export PATH=\$JAVA_HOME/bin:\$PATH\" | tee -a \"/etc/profile.d/00-java.sh\"
+  - echo \"export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64\" | tee -a \"/etc/profile\"
+  - echo \"export PATH=\$JAVA_HOME/bin:\$PATH\" | tee -a \"/etc/profile\"
+  - touch /home/tomcat/.bash_profile
+  - echo \"export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64\" | tee -a \"/home/tomcat/.bash_profile\"
+  - echo \"export PATH=\$JAVA_HOME/bin:\$PATH\" | tee -a \"/home/tomcat/.bash_profile\"
+  - touch /home/tomcat/.bashrc
+  - echo \"export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64\" | tee -a \"/home/tomcat/.bashrc\"
+  - echo \"export PATH=\$JAVA_HOME/bin:\$PATH\" | tee -a \"/home/tomcat/.bashrc\"
+  - touch /home/${username}/.bash_profile
+  - echo \"export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64\" | tee -a \"/home/${username}/.bash_profile\"
+  - echo \"export PATH=\$JAVA_HOME/bin:\$PATH\" | tee -a \"/home/${username}/.bash_profile\"
+  - touch /home/${username}/.bashrc
+  - echo \"export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64\" | tee -a \"/home/${username}/.bashrc\"
+  - echo \"export PATH=\$JAVA_HOME/bin:\$PATH\" | tee -a \"/home/${username}/.bashrc\"
   - (cd /tmp && curl -O http://apache.mirrors.tds.net/tomcat/tomcat-9/v9.0.16/bin/apache-tomcat-9.0.16.tar.gz)
   - mkdir /opt/tomcat
   - tar xzvf /tmp/apache-tomcat-9.0.16.tar.gz -C /opt/tomcat --strip-components=1
@@ -37,8 +53,6 @@ runcmd:
   - mysql < /tmp/create.sql
   - curl -o /tmp/SALESMANAGER.sql ${salesmanager}
   - mysql SALESMANAGER < /tmp/SALESMANAGER.sql
-  - (cd /opt/Shopizer && mvn clean install)
+  - (cd /opt/Shopizer && JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64 mvn clean install)
   - cp /opt/Shopizer/sm-shop/target/ROOT.war /opt/tomcat/webapps/
   - chown tomcat:tomcat /opt/tomcat/webapps/ROOT.war
-  - reboot
-  
